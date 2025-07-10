94817caa6a9f33451a6e6d958d6e0b3d6f33be4e
Levente0324 <leventedukay@gmail.com>
2025-07-10 03:41:48 +0200

    fghfgh

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,6 @@
-/** @type {import('postcss-load-config').Config} */
 module.exports = {
   plugins: {
-    // Use our custom resolver to ensure tailwindcss is always found
-    [require.resolve("./tailwind-resolver.js")]: {},
+    tailwindcss: {},
     autoprefixer: {},
   },
 };
